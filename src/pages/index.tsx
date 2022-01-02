71d6d49784e3e6ddf8066aee56e345bc0ae454fe
Carlos Amorim <carlos.av.amorim@gmail.com>
2022-01-01 23:19:45 -0300

    Header links e favicon finalizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-//Para alterar o título individualmente em cada página, e qualquer coisa no head "description" também
 import Head from 'next/head' 
-
 
 export default function Home() {
   return (
