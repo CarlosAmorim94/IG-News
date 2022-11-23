618ea45d9316987e020117fbc52e0671cc499a46
carlos <carlos.av.amorim@gmail.com>
2022-11-23 15:03:44 -0300

    Fix: Fix the types of providers on authentication button with NextAuth and Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
